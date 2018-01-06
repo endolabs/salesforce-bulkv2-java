3c53f7caa1e6d3fb1bea145b76ee1d1b20ca404c
Takumi Endo <tq-ed@v7.com>
2018-01-06 13:24:53 +0900

    Remove redundant code in requester

--- src/main/java/bulk2j/RestRequester.java
+++ src/main/java/bulk2j/RestRequester.java
@@ -38,8 +38,6 @@ public class RestRequester {
     }
 
     public <T> T postMultiPart(String url, Object requestData, Class<T> responseClass) {
-        HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse(url)).newBuilder();
-
         String sampleAccountsCsv = "Name,Description,NumberOfEmployees\n" +
                 "TestAccount1,Description of TestAccount1,30\n" +
                 "TestAccount2,Another description,40\n" +
@@ -53,65 +51,13 @@ public class RestRequester {
                         RequestBody.create(CSV_MEDIA_TYPE, sampleAccountsCsv))
                 .build();
 
-        Request request = new Request.Builder()
-                .url(urlBuilder.build())
-                .method("POST", requestBody)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            ResponseBody responseBody = response.body();
-            if (responseBody == null) {
-                throw new RuntimeException("response body is null.");
-            }
-
-            String responseJson = responseBody.string();
-            log.info("{} response {} {}", "POST", response.code(), responseJson);
-
-            if (response.isSuccessful()) {
-                return Json.decode(responseJson, responseClass);
-            } else {
-                List<ErrorResponse> errors = Json.decode(responseJson, ERRORS_TYPE_REFERENCE);
-                log.error("error : {}", errors);
-                throw new RuntimeException("error.");
-            }
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
+        return request(url, "POST", new HashMap<>(), requestBody, responseClass);
     }
 
     public <T> T putCsv(String url, String requestData, Class<T> responseClass) {
-        HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse(url)).newBuilder();
-
         RequestBody requestBody = (requestData == null) ? null : RequestBody.create(CSV_MEDIA_TYPE, ByteString.encodeUtf8(requestData));
 
-        Request request = new Request.Builder()
-                .url(urlBuilder.build())
-                .method("PUT", requestBody)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            ResponseBody responseBody = response.body();
-            if (responseBody == null) {
-                throw new RuntimeException("response body is null.");
-            }
-
-            String responseJson = responseBody.string();
-            log.info("{} response {} {}", "POST", response.code(), responseJson);
-
-            if (response.isSuccessful()) {
-                if (responseJson.isEmpty()) {
-                    return null;
-                } else {
-                    return Json.decode(responseJson, responseClass);
-                }
-            } else {
-                List<ErrorResponse> errors = Json.decode(responseJson, ERRORS_TYPE_REFERENCE);
-                log.error("error : {}", errors);
-                throw new RuntimeException("error.");
-            }
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
+        return request(url, "PUT", new HashMap<>(), requestBody, responseClass);
     }
 
     public <T> T get(String url, Class<T> responseClass) {
@@ -123,26 +69,30 @@ public class RestRequester {
     }
 
     public <T> T post(String url, Object requestData, Class<T> responseClass) {
-        return request(url, "POST", new HashMap<>(), requestData, responseClass);
+        return requestJson(url, "POST", new HashMap<>(), requestData, responseClass);
     }
 
     public <T> T put(String url, Object requestData, Class<T> responseClass) {
-        return request(url, "PUT", new HashMap<>(), requestData, responseClass);
+        return requestJson(url, "PUT", new HashMap<>(), requestData, responseClass);
     }
 
     public <T> T delete(String url, Object requestData, Class<T> responseClass) {
-        return request(url, "DELETE", new HashMap<>(), requestData, responseClass);
+        return requestJson(url, "DELETE", new HashMap<>(), requestData, responseClass);
     }
 
     public <T> T patch(String url, Object requestData, Class<T> responseClass) {
-        return request(url, "PATCH", new HashMap<>(), requestData, responseClass);
+        return requestJson(url, "PATCH", new HashMap<>(), requestData, responseClass);
     }
 
-    private <T> T request(String url, String httpMethod, Map<String, String> queryParams, Object requestData, Class<T> responseClass) {
+    private <T> T requestJson(String url, String httpMethod, Map<String, String> queryParams, Object requestData, Class<T> responseClass) {
+        RequestBody requestBody = (requestData == null) ? null : RequestBody.create(JSON_MEDIA_TYPE, Json.encode(requestData));
+
+        return request(url, httpMethod, queryParams, requestBody, responseClass);
+    }
+
+    private <T> T request(String url, String httpMethod, Map<String, String> queryParams, RequestBody requestBody, Class<T> responseClass) {
         HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse(url)).newBuilder();
         queryParams.forEach(urlBuilder::addQueryParameter);
-
-        RequestBody requestBody = (requestData == null) ? null : RequestBody.create(JSON_MEDIA_TYPE, Json.encode(requestData));
 
         Request request = new Request.Builder()
                 .url(urlBuilder.build())
@@ -159,7 +109,11 @@ public class RestRequester {
             log.info("{} response {} {}", httpMethod, response.code(), responseJson);
 
             if (response.isSuccessful()) {
-                return Json.decode(responseJson, responseClass);
+                if (responseJson.isEmpty()) {
+                    return null;
+                } else {
+                    return Json.decode(responseJson, responseClass);
+                }
             } else {
                 List<ErrorResponse> errors = Json.decode(responseJson, ERRORS_TYPE_REFERENCE);
                 log.error("error : {}", errors);
